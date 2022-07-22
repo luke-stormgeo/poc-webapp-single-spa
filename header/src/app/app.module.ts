be44f978132183ce4835f6f6b8ce0fc2fc5394a4
Luke Angell <luke.angell@stormgeo.com>
2022-07-22 14:12:21 +0200

    feat(header.component): add header component

--- header/src/app/app.module.ts
+++ header/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, EmptyRouteComponent],
+  declarations: [AppComponent, EmptyRouteComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
